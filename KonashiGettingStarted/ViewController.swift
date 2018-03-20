17a445bdee332a5fe3e7617626befb4caa54ada8
Minori Manabe <minori@Minori.local>
2018-03-20 18:01:28 +0900

    first commit

--- KonashiGettingStarted/ViewController.swift
+++ KonashiGettingStarted/ViewController.swift
@@ -7,19 +7,30 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        Konashi.shared().readyHandler = {() -> Void in
+            Konashi.pinMode(KonashiDigitalIOPin.LED2, mode: KonashiPinMode.output)
+            Konashi.digitalWrite(KonashiDigitalIOPin.LED2, value: KonashiLevel.high)
+            Konashi.pinMode(KonashiDigitalIOPin.S1,mode:KonashiPinMode.input)
+            Konashi.shared().digitalInputDidChangeValueHandler = {_,_  in
+                print("sw=",  Konashi.digitalRead(KonashiDigitalIOPin.S1))
+            }
+        }
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func find(sender: UIButton) {
+        Konashi.find()
+    }
 }
-
